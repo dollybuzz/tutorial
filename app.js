8d0f520aa9639492cd617871d96fbde3cb6beaf9
Dalia <43527456+dollybuzz@users.noreply.github.com>
2019-12-02 06:26:53 +0000

    added bcrypt

--- app.js
+++ app.js
@@ -23,12 +23,17 @@ app.get("/", function(req, res){
 });
 
 //root post route
-app.post("/", function(req, res){
+app.post("/", async function(req, res){
     let username = req.body.username;
     let password = req.body.password;
-    console.log("Username:" + username);
-    console.log("password:" + password);
+    //console.log("Username:" + username);
+    //console.log("password:" + password);
     //res.send("This is the root route using POST!");
+    let hashedPwd = "$2a$10$06ofFgXJ9wysAOzQh0D0..RcDp1w/urY3qhO6VuUJL2c6tzAJPfj6";
+    
+    let passwordMatch = await checkPassword(password, hashedPwd);
+    console.log("passwordMatch: " + passwordMatch);
+    
     if(username == "admin" && password == "secret") {
         res.render("welcome.ejs");
     } else {
@@ -36,6 +41,20 @@ app.post("/", function(req, res){
         //passing loginError from route to view index.ejs
     }
 });
+
+/*
+Checks the bcrypt value of the password submitted
+@param {string} password
+@return {boolean} true if password sub mitted is equal to bcrypt-hashed value, false otherwise.
+*/
+function checkPassword(password, hashedValue) {
+    return new Promise( function(resolve, reject) {
+        bcrypt.compare(password, hashedValue, function(err, result) {
+            console.log("Result: " + result);
+            resolve(result);
+        });
+    });
+}
 
 //listener
 app.listen("8080", "127.0.0.1", function(){
